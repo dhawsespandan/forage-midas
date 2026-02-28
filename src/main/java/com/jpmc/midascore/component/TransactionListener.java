434b291c6938acab3c9e283147862a7bcd70296e
Spandan <dhawsespandan@gmail.com>
2026-02-28 13:09:13 +0530

    REST API Integration

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -2,21 +2,25 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class TransactionListener {
 
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
 
     public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository) {
         this.userRepository = userRepository;
         this.transactionRepository = transactionRepository;
+        this.restTemplate = new RestTemplate();
     }
 
     @KafkaListener(topics = "${general.kafka-topic}")
@@ -27,14 +31,22 @@ public class TransactionListener {
         if (sender == null || recipient == null) return;
         if (sender.getBalance() < transaction.getAmount()) return;
 
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:8080/incentive",
+                transaction,
+                Incentive.class
+        );
+
+        float incentiveAmount = (incentive != null) ? incentive.getAmount() : 0;
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
         userRepository.save(sender);
         userRepository.save(recipient);
-        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount));
 
-        UserRecord waldorf = userRepository.findByName("waldorf");
-        if (waldorf != null) System.out.println("Waldorf balance: " + waldorf.getBalance());
+        UserRecord wilbur = userRepository.findByName("wilbur");
+        if (wilbur != null) System.out.println("Wilbur balance: " + wilbur.getBalance());
     }
 }
